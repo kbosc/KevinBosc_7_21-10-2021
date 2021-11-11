141325081dea6bb700cc524307b6b83652e1378e
Kevin Bosc <kevin_bosc@hotmail.fr>
2021-11-11 21:53:46 +0100

    dropdown implémenter, manque la liste à mettre dynamique et le clique with this

--- public/js/components/dropDownDisplay/index.js
+++ public/js/components/dropDownDisplay/index.js
@@ -1,15 +1,17 @@
 const dropdown = document.querySelector(".dropdown__ingredient");
 const blockLinks = document.querySelector(".dropdown__ingredient .block-links");
 // const blockLinks = document.querySelectorAll(".block-links");
-const btnIngredient = document.querySelector(".btn-ingredient");
-// const liItems = document.querySelectorAll(".dropdown li")
 const liIngredient = document.querySelectorAll(".dropdown__ingredient li");
-
+// const btnIngredient = document.querySelector(".btn-ingredient");
+const btnIngredient = document.querySelectorAll(".dpd button");
+// liIngredient.forEach((li) => li.addEventListener("click", toggleDropDown));
 let toggleIndex;
-
-btnIngredient.addEventListener("click", toggleDropDown);
+btnIngredient.forEach((btn) => btn.addEventListener("click", toggleDropDown));
+// btnIngredient.addEventListener("click", toggleDropDown);
 function toggleDropDown() {
   const icon = this.querySelector("img");
+  //   const blockLinks = this.querySelector(".block-links");
+  //   console.log(blockLinks);
   if (!toggleIndex) {
     blockLinks.style.display = "block";
     blockLinks.style.height = `${blockLinks.scrollHeight}px`;
@@ -21,6 +23,21 @@ function toggleDropDown() {
   toggleIndex = false;
   icon.classList.toggle("rotate");
 }
+
+// btnAppareil.addEventListener("click", toggleDropDown);
+// function toggleDropDown() {
+//   const icon = this.querySelector("img");
+//   if (!toggleIndex) {
+//     blockLinks.style.display = "block";
+//     blockLinks.style.height = `${blockLinks.scrollHeight}px`;
+//     toggleIndex = true;
+//     icon.classList.toggle("rotate");
+//     return;
+//   }
+//   blockLinks.style.height = 0;
+//   toggleIndex = false;
+//   icon.classList.toggle("rotate");
+// }
 ////////////////////////////////////////////////
 // Ferme la dropdown si clique sur un li
 // liIngredient.forEach((li) => li.addEventListener("click", toggleDropDown));
